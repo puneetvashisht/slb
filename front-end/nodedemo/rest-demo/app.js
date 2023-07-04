70eec44cfd7b4485b91387a94b081163c4ef14f5
Puneet Vashisht <puneetvashisht@gmail.com>
2023-07-04 11:50:17 +0530

    rest-api

---
+++ front-end/nodedemo/rest-demo/app.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+
+const app = express();
+
+const employees = [
+    {id: 3, name: "Ravi", salary: 333344},
+    {id:2, name: "Priya", salary: 333343}
+]
+
+app.get('/employees', (req, res)=> {
+    res.json(employees)
+})
+
+app.get('/hello', (req, res)=> {
+    res.send('hello world')
+    res.end();
+})
+
+app.listen(3000, ()=> console.log('listening on port 3000..'))
