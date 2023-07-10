d2a099efa0c43b8c07e136192fa628430ce80bcd
Puneet Vashisht <puneetvashisht@gmail.com>
2023-07-10 14:40:33 +0530

    pipes

--- front-end/angular/my-third-app/src/app/app.module.ts
+++ front-end/angular/my-third-app/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ViewPostsComponent } from './view-posts/view-posts.component';
 import {HttpClientModule} from '@angular/common/http';
-import { AddPostComponent } from './add-post/add-post.component'
+import { AddPostComponent } from './add-post/add-post.component';
+import { CapitalizePipe } from './capitalize.pipe';
+import { SearchPipe } from './search.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewPostsComponent,
-    AddPostComponent
+    AddPostComponent,
+    CapitalizePipe,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
---
+++ front-end/angular/my-third-app/src/app/search.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Post } from './model/Post';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  transform(posts: Array<Post>, ...args: Array<string>): Array<Post> {
+    let searchedPosts = posts.filter(post => post.title.startsWith(args[0]))
+    return searchedPosts;
+  }
+
+}
